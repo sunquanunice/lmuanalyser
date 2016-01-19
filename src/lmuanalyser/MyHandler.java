449c0dc702a9938f123f386ec737c3f738a87eec
SUN QUAN <sunquanmw@gmail.com>
2016-01-19 01:00:39 +0100

    Added the project and the package in the package explorer

--- src/lmuanalyser/MyHandler.java
+++ src/lmuanalyser/MyHandler.java
@@ -1,5 +1,6 @@
 package lmuanalyser;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -35,11 +36,11 @@ public class MyHandler extends AbstractHandler {
 			IResource resource = (IResource) Platform.getAdapterManager().getAdapter(selected, IResource.class);
 			String path = resource.getLocation().toString();
 			MyFrame myFrame = new MyFrame(path);
-		} else if(selected instanceof IProject) {
+		} else if (selected instanceof IProject) {
 			IResource resource = (IResource) Platform.getAdapterManager().getAdapter(selected, IResource.class);
 			String path = resource.getLocation().toString();
 			MyFrame myFrame = new MyFrame("loadProject " + path);
-		} else if(selected instanceof IFolder) {
+		} else if (selected instanceof IFolder) {
 			IResource resource = (IResource) Platform.getAdapterManager().getAdapter(selected, IResource.class);
 			String path = resource.getLocation().toString();
 			MyFrame myFrame = new MyFrame("loadFolder " + path);
@@ -47,22 +48,33 @@ public class MyHandler extends AbstractHandler {
 			IResource resource = (IResource) Platform.getAdapterManager().getAdapter(selected, IResource.class);
 			IPath path = resource.getLocation();
 			String spath = path.toString();
-			if(resource.getFileExtension() != null && (resource.getFileExtension().equals("java") || resource.getFileExtension().equals("jar"))) {
+			if (resource.getFileExtension() != null
+					&& (resource.getFileExtension().equals("java") || resource.getFileExtension().equals("jar"))) {
 				MyFrame myFrame = new MyFrame(spath);
 			}
-			IProject project = (IProject)Platform.getAdapterManager().getAdapter(selected, IProject.class);
-			resource = (IResource) Platform.getAdapterManager().getAdapter(project, IResource.class); 
-			path = resource.getLocation();
-			System.err.println(path);
-		} else  {
-			System.err.println(selected.getClass());
-		}
-		if (selected instanceof IFolder || selected instanceof PlatformObject) {
-			IResource resource = (IResource) Platform.getAdapterManager().getAdapter(selected, IResource.class);
-			IPath path = resource.getLocation();
-
-	
 			
+			try {
+				IProject project = (IProject) Platform.getAdapterManager().getAdapter(selected, IProject.class);
+				resource = (IResource) Platform.getAdapterManager().getAdapter(project, IResource.class);
+				path = resource.getLocation();
+				new MyFrame("loadProject " + path);
+			} catch (Exception e) {
+				resource = (IResource) Platform.getAdapterManager().getAdapter(selected, IResource.class);
+				 path = resource.getLocation();
+				 //Verify if the folder is in the src or not
+				/* spath = path.toString();
+				 String binPath = "";
+				 if(spath.endsWith("src")) {
+					 binPath = spath.substring(0, spath.length()-3);
+				 } else {
+					 int i = spath.indexOf("/src/");
+					 System.err.println(spath + "    " + File.separator);
+					 //binPath = spath.substring(0, index);
+				 }*/
+					MyFrame myFrame = new MyFrame("loadFolder " + path);
+			}
+		} else {
+			System.err.println(selected.getClass());
 		}
 		return null;
 	}
